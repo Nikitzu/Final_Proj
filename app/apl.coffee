e114eeac5af8bca8f157ef2e44f0c787f69f78f2
Immorrtall <artur.nehaichik@yandex.ru>
2016-02-05 20:12:08 +0300

    update user

--- app/apl.coffee
+++ app/apl.coffee
@@ -3,4 +3,14 @@ angular.module('myApp', [
   'myApp.version'
 ]).controller 'MyCtr', ($scope)->
   $scope.world = 'Nik'
+  $scope.changeButton_text = "Change settings"
+  $scope.buttonSettings = false
+  $scope.changeSettings = ->
+    if $scope.buttonSettings == false
+      $scope.buttonSettings = true
+      $scope.changeButton_text = "Save changes"
+    else
+      $scope.buttonSettings = false
+      $scope.changeButton_text = "Change settings"
+    return
   return
--- js/app/apl.js
+++ js/app/apl.js
@@ -2,6 +2,17 @@
 (function() {
   angular.module('myApp', ['ngRoute', 'myApp.version']).controller('MyCtr', function($scope) {
     $scope.world = 'Nik';
+    $scope.changeButton_text = "Change settings";
+    $scope.buttonSettings = false;
+    $scope.changeSettings = function() {
+      if ($scope.buttonSettings === false) {
+        $scope.buttonSettings = true;
+        $scope.changeButton_text = "Save changes";
+      } else {
+        $scope.buttonSettings = false;
+        $scope.changeButton_text = "Change settings";
+      }
+    };
   });
 
 }).call(this);
