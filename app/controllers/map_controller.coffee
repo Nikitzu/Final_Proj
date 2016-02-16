8d1194825a71cd05093e26a18e81af91410dd922
Immorrtall <artur.nehaichik@yandex.ru>
2016-02-16 18:25:18 +0300

    full map

--- app/controllers/map_controller.coffee
+++ app/controllers/map_controller.coffee
@@ -2,15 +2,43 @@ app = angular.module 'myApp'
 
 app.config (uiGmapGoogleMapApiProvider) ->
   uiGmapGoogleMapApiProvider.configure
-    v: '3.20'
+    v: '3'
     libraries: 'weather,geometry,visualization'
   return
 
-app.controller 'mapCtrl', ($scope) ->
-  $scope.map =
-    center:
-      latitude: 51.219053
-      longitude: 4.404418
-    zoom: 14
-  $scope.options = scrollwheel: false
+app.controller 'mapCtrl', ($scope, $window, uiGmapGoogleMapApi) ->
+  $window.navigator.geolocation.getCurrentPosition (position) ->
+    $scope.latitude = position.coords.latitude
+    $scope.longitude = position.coords.longitude
+    console.log($scope.latitude, $scope.longitude)
+  $scope.mapReady= ->
+    uiGmapGoogleMapApi.then ->
+      $scope.map =
+        center:
+          latitude: $scope.latitude
+          longitude: $scope.longitude
+        zoom: 5
+        markers: []
+        events: click: (map, eventName, originalEventArgs) ->
+          e = originalEventArgs[0]
+          lat = e.latLng.lat()
+          lon = e.latLng.lng()
+          marker =
+            id: Date.now()
+            coords:
+              latitude: lat
+              longitude: lon
+          $scope.map.markers[0] = marker
+          console.log $scope.map.markers
+          $scope.$apply()
+          return
+      $scope.options = scrollwheel: false
+
+      $scope.marker =
+        id: 0
+        coords:
+          latitude: $scope.latitude
+          longitude: $scope.longitude
+        options:
+          icon: '../../source/assets/img/blue_marker.png'
   return
--- js/app/controllers/map_controller.js
+++ js/app/controllers/map_controller.js
@@ -6,21 +6,59 @@
 
   app.config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-      v: '3.20',
+      v: '3',
       libraries: 'weather,geometry,visualization'
     });
   });
 
-  app.controller('mapCtrl', function($scope) {
-    $scope.map = {
-      center: {
-        latitude: 51.219053,
-        longitude: 4.404418
-      },
-      zoom: 14
-    };
-    $scope.options = {
-      scrollwheel: false
+  app.controller('mapCtrl', function($scope, $window, uiGmapGoogleMapApi) {
+    $window.navigator.geolocation.getCurrentPosition(function(position) {
+      $scope.latitude = position.coords.latitude;
+      $scope.longitude = position.coords.longitude;
+      return console.log($scope.latitude, $scope.longitude);
+    });
+    $scope.mapReady = function() {
+      return uiGmapGoogleMapApi.then(function() {
+        $scope.map = {
+          center: {
+            latitude: $scope.latitude,
+            longitude: $scope.longitude
+          },
+          zoom: 5,
+          markers: [],
+          events: {
+            click: function(map, eventName, originalEventArgs) {
+              var e, lat, lon, marker;
+              e = originalEventArgs[0];
+              lat = e.latLng.lat();
+              lon = e.latLng.lng();
+              marker = {
+                id: Date.now(),
+                coords: {
+                  latitude: lat,
+                  longitude: lon
+                }
+              };
+              $scope.map.markers[0] = marker;
+              console.log($scope.map.markers);
+              $scope.$apply();
+            }
+          }
+        };
+        $scope.options = {
+          scrollwheel: false
+        };
+        return $scope.marker = {
+          id: 0,
+          coords: {
+            latitude: $scope.latitude,
+            longitude: $scope.longitude
+          },
+          options: {
+            icon: '../../source/assets/img/blue_marker.png'
+          }
+        };
+      });
     };
   });
 
