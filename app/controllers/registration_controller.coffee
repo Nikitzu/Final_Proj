e52a0b44ae1fb0e0284923cf621e9b077f8cd1da
Immorrtall <artur.nehaichik@yandex.ru>
2016-02-20 20:25:02 +0300

    dialog window if pass or login is incorrect

--- app/controllers/registration_controller.coffee
+++ app/controllers/registration_controller.coffee
@@ -42,7 +42,7 @@ app.directive 'popUpMsg', ->
   {
     restrict: 'E'
     scope: false
-    template: '<div id="popUpMsg-bg" ng-show="showPopUpMsg"><div id="popUpMsg"><div class="close" ng-click="closePopUp()">x</div><div class="content">{{popUpMsgContent}}</div><button ng-click="closePopUp()">Ok</button></div></div>'
+    template: '<div id="popUpMsg-bg" ng-show="showPopUpMsg"><div id="popUpMsg"><div class="content">{{popUpMsgContent}}</div><button ng-click="closePopUp()">Ok</button></div></div>'
     controller: ($scope) ->
 
       $scope.closePopUp = ->
--- js/app/controllers/registration_controller.js
+++ js/app/controllers/registration_controller.js
@@ -46,7 +46,7 @@
     return {
       restrict: 'E',
       scope: false,
-      template: '<div id="popUpMsg-bg" ng-show="showPopUpMsg"><div id="popUpMsg"><div class="close" ng-click="closePopUp()">x</div><div class="content">{{popUpMsgContent}}</div><button ng-click="closePopUp()">Ok</button></div></div>',
+      template: '<div id="popUpMsg-bg" ng-show="showPopUpMsg"><div id="popUpMsg"><div class="content">{{popUpMsgContent}}</div><button ng-click="closePopUp()">Ok</button></div></div>',
       controller: function($scope) {
         $scope.closePopUp = function() {
           $scope.showPopUpMsg = false;
